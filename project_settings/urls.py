c74591297a0b0d32844952ff5263519e20391d18
Tim Jagodzinski <tim.jagodzinski@mtc-berlin.com>
2012-09-24 15:50:03 +0200

    adds base.html template and accompanying script filse, css

--- project_settings/urls.py
+++ project_settings/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'project_settings.views.home', name='home'),
+    url(r'^$', 'teamhub.views.dashboard', name='home'),
     # url(r'^project_settings/', include('project_settings.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- teamhub/views.py
+++ teamhub/views.py
@@ -1,7 +1,18 @@
+# coding: utf-8
+
+from django.shortcuts import render_to_response
+from teamhub.models import Aufgabe
 # Create your views here.
 
-def alle_aufgaben(request):
-    pass
+def dashboard(request):
+    '''
+    Erstellt die Dashboardansicht mit den dafür nötigen Daten:
+        * Meine zugewiesenen Tickets
+        * Meine zugewiesenen Projekte
+    '''
+    meineAufgaben = Aufgabe.objects.filter(bearbeiter = request.user).order_by('-faelligkeitsDatum')
+    context = {'meineAufgaben': meineAufgaben}
+    return render_to_response('../templates/base.html', context)
 
 def user_profile(request):
     pass
