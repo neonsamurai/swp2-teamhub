86697369c358790516045ac329455fe5d6fe6268
Tim Jagodzinski <tim.jagodzinski@mtc-berlin.com>
2012-09-27 19:37:45 +0200

    removes South datamigration due to incompatibility to sqlite3

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'django.contrib.comments',
-    'south',
+    #'south',
     'teamhub',
 )
 
