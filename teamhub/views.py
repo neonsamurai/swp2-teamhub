5a2e04c200fb8e45ea7c3c9a6ba28fba924dfc29
Tim Jagodzinski <tim@ubuntu.(none)>
2012-10-12 15:08:12 +0200

    adds logout for users

--- teamhub/views.py
+++ teamhub/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.contrib.auth.views import logout_then_login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from teamhub.models import Aufgabe
@@ -15,6 +16,12 @@ def dashboard(request):
     context = {'meineAufgaben': meineAufgaben}
     return render_to_response('../templates/base.html', context)
 
+def logoutUser(request):
+    '''
+    Meldet den Anwender vom System ab und leitet auf die Login-Seite weiter.
+    '''
+    return logout_then_login(request, '/login/')
+
 def aufgabeDetails(request, aufgabeId):
     '''
     Erstellt die Detailansicht für eine Aufgabe.
@@ -22,3 +29,11 @@ def aufgabeDetails(request, aufgabeId):
     aufgabe = Aufgabe.objects.get(pk=aufgabeId)
     context = {'aufgabe': aufgabe}
     return render_to_response('../templates/base_aufgabe.html', context)
+
+def userProfilBearbeiten(request):
+    '''
+    Erstellt die Bearbeitungsansicht für das Profil des angemeldeten Benutzers.
+    '''
+    profil = User.objects.get(pk=request.user.id)
+    context = {'profil': profil}
+    return render_to_response('../templates/base_profil.html', context)
