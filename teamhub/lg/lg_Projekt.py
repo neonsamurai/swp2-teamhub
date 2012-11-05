902f5c71c016729c61c1d899c60ba19e18aeff60
D. Lipps <dennislipps@googlemail.com>
2012-11-05 21:51:24 +0100

    Logikschicht vol.3

--- teamhub/lg/lg_Projekt.py
+++ teamhub/lg/lg_Projekt.py
@@ -3,7 +3,7 @@ from teamhub.models import Projekt
 
 class lgProjekt:
     def lg_projekt_isValid(self, projekt):
-        if Projekt.objects.filter(name=projekt.name).count()!=0:
+        if Projekt.objects.filter(name=projekt.name).exclude(pk=projekt.pk).count()!=0:
             return False
         projekt.save()
         return True
