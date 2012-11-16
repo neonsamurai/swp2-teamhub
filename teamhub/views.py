0131d961797b571ea8b94ecefe85372e6dc5c46b
Veronika <veronika.gross@arcor.de>
2012-11-16 13:26:16 +0100

    paar fehler beseitigt

--- teamhub/views.py
+++ teamhub/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
-from teamhub.models import Aufgabe, Projekt, CustomUser
+from teamhub.models import Aufgabe, Projekt
 # Create your views here.
 
 
