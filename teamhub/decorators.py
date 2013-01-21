657d054e29d4142d1231ceef34f657efc1baa809
dlipps <dennislipps@googlemail.com>
2013-01-21 20:55:50 +0100

    Bug mit der Statusanzeige nach einem Fehler

--- teamhub/decorators.py
+++ teamhub/decorators.py
@@ -1,22 +1,29 @@
 # coding: utf-8
 
-from teamhub.models import Aufgabe, Projekt
+from teamhub.models import Aufgabe, Projekt, AUFGABE_STATUS
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.shortcuts import render_to_response, redirect
 from django.core.context_processors import csrf
 from django.template import RequestContext
 import teamhub.stringConst as c
+import re
 
 
 
 from teamhub.models import TeamhubUser
 
 
-def makeContext(context):
+def makeContext(context, request):
     context['projektliste'] = Projekt.objects.all().order_by('name')
     context['prioritaet'] = [c.PRIORITAET_HI, c.PRIORITAET_ME, c.PRIORITAET_LO]
-
+    pattern = re.compile(r'^/aufgabe/(?P<aufgabeId>\d+)/bearbeiten/$')
+    treffer=re.match(pattern, request.path)
+    if treffer:
+        aufgabe=Aufgabe.objects.get(pk=treffer.group('aufgabeId'))
+        context['stati']=aufgabe.getStati()
+        context['aktuellerstatus_lang']=dict(AUFGABE_STATUS)[aufgabe.status]
+    return context
 
 def decorateSave(func):
     def wrapper(form, request, template):
@@ -36,13 +43,14 @@ def decorateSave(func):
                 form._errors["faelligkeitsDatum"] = form.error_class([msg])
                 del form.cleaned_data["faelligkeitsDatum"]
                 context['form'] = form
-            if msg == c.FEHLER_AUFGABE_PROJEKTSTATUS or msg == c.FEHLER_AUFGABE_STATUS:
+            if msg == c.FEHLER_AUFGABE_PROJEKTSTATUS:
                 form._errors['__all__'] = form.error_class([msg])
                 context['form'] = form
             if msg == c.FEHLER_TEAMHUBUSER_USERNAME_INVALID:
                 form._errors["username"] = form.error_class([msg])
                 del form.cleaned_data["username"]
                 context['form'] = form
+            context = makeContext(context, request)
             return render_to_response(template, context, context_instance=RequestContext(request))
 
         except Exception, e:
@@ -51,7 +59,7 @@ def decorateSave(func):
             context.update(csrf(request))
             form._errors['__all__'] = form.error_class([msg])
             context['form'] = form
-            context = makeContext(context)
+            context = makeContext(context, request)
             return render_to_response(template, context, context_instance=RequestContext(request))
     return wrapper
 
